fa3d109884d86640f948dd0f2159ba1fbb522f58
Jason Desiderio <jmdesiderio@gmail.com>
2022-11-25 20:30:32 -0600

    Generate contact page

--- web/src/Routes.tsx
+++ web/src/Routes.tsx
@@ -12,6 +12,7 @@ import { Router, Route } from '@redwoodjs/router'
 const Routes = () => {
   return (
     <Router>
+      <Route path="/contact" page={ContactPage} name="contact" />
       <Route notfound page={NotFoundPage} />
     </Router>
   )
