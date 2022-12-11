37c9165f0f79127b56f82374e3ab86e9d6b1da22
Jason Desiderio <jmdesiderio@gmail.com>
2022-12-11 00:26:05 -0600

    Add user account dashboard display

--- api/src/services/users/users.ts
+++ api/src/services/users/users.ts
@@ -1,8 +1,11 @@
 import type { QueryResolvers } from 'types/graphql'
 
+import { isOwner } from 'src/lib/auth'
 import { db } from 'src/lib/db'
 
 export const user: QueryResolvers['user'] = ({ id }) => {
+  isOwner(id)
+
   return db.user.findUnique({
     where: { id },
   })
---
+++ web/src/components/AccountCell/AccountCell.tsx
@@ -0,0 +1,28 @@
+import type { FindAccountQuery, FindAccountQueryVariables } from 'types/graphql'
+
+import type { CellSuccessProps, CellFailureProps } from '@redwoodjs/web'
+
+export const QUERY = gql`
+  query FindAccountQuery($id: Int!) {
+    user(id: $id) {
+      id
+      email
+    }
+  }
+`
+
+export const Loading = () => <div>Loading...</div>
+
+export const Empty = () => <div>Empty</div>
+
+export const Failure = ({
+  error,
+}: CellFailureProps<FindAccountQueryVariables>) => (
+  <div style={{ color: 'red' }}>Error: {error?.message}</div>
+)
+
+export const Success = ({
+  user,
+}: CellSuccessProps<FindAccountQuery, FindAccountQueryVariables>) => {
+  return <div>{JSON.stringify(user)}</div>
+}
