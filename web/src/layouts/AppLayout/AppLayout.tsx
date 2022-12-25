08afaa43e45000f6715c79af844b3606dd91b32f
Jason Desiderio <jmdesiderio@gmail.com>
2022-12-25 16:56:07 -0500

    Adjust auth buttons in nav bar

--- web/src/layouts/AppLayout/AppLayout.tsx
+++ web/src/layouts/AppLayout/AppLayout.tsx
@@ -62,53 +62,63 @@ const AppLayout = ({ children }: AppLayoutProps) => {
             </HStack>
           </HStack>
           <Flex alignItems={'center'}>
-            <HStack mr="4" dir="horizontal">
-              {isAuthLoading ? (
-                <Spinner />
-              ) : isAuthenticated ? (
-                <>
-                  <Link to={routes.home()}>
-                    Logged in as {currentUser.email}
-                  </Link>{' '}
-                  <Button
-                    variant={'solid'}
-                    colorScheme={'teal'}
-                    size={'sm'}
-                    mr={4}
-                    onClick={onClickLogout}
-                  >
-                    Logout
-                  </Button>
-                </>
-              ) : (
-                <>
-                  <Link to={routes.signUp()}>Sign Up</Link>
-                  <Link to={routes.logIn()}>Log In</Link>
-                </>
-              )}
-            </HStack>
-            <Menu>
-              <MenuButton
-                as={Button}
-                rounded={'full'}
-                variant={'link'}
-                cursor={'pointer'}
-                minW={0}
-              >
-                <Avatar
-                  size={'sm'}
-                  src={
-                    'https://images.unsplash.com/photo-1493666438817-866a91353ca9?ixlib=rb-0.3.5&q=80&fm=jpg&crop=faces&fit=crop&h=200&w=200&s=b616b2c5b373a80ffc9636ba24f7a4a9'
-                  }
-                />
-              </MenuButton>
-              <MenuList>
-                <MenuItem>Link 1</MenuItem>
-                <MenuItem>Link 2</MenuItem>
-                <MenuDivider />
-                <MenuItem>Link 3</MenuItem>
-              </MenuList>
-            </Menu>
+            {isAuthLoading ? (
+              <Spinner />
+            ) : (
+              <HStack spacing={3} dir="horizontal">
+                {isAuthenticated ? (
+                  <>
+                    <Link to={routes.home()}>
+                      Logged in as {currentUser.email}
+                    </Link>{' '}
+                    <Button
+                      variant={'solid'}
+                      colorScheme={'teal'}
+                      size={'sm'}
+                      mr={4}
+                      onClick={onClickLogout}
+                    >
+                      Logout
+                    </Button>
+                    <Menu>
+                      <MenuButton
+                        as={Button}
+                        rounded={'full'}
+                        variant={'link'}
+                        cursor={'pointer'}
+                        minW={0}
+                      >
+                        <Avatar
+                          size={'sm'}
+                          src={
+                            'https://images.unsplash.com/photo-1493666438817-866a91353ca9?ixlib=rb-0.3.5&q=80&fm=jpg&crop=faces&fit=crop&h=200&w=200&s=b616b2c5b373a80ffc9636ba24f7a4a9'
+                          }
+                        />
+                      </MenuButton>
+                      <MenuList>
+                        <MenuItem>Link 1</MenuItem>
+                        <MenuItem>Link 2</MenuItem>
+                        <MenuDivider />
+                        <MenuItem>Link 3</MenuItem>
+                      </MenuList>
+                    </Menu>
+                  </>
+                ) : (
+                  <>
+                    <Link to={routes.logIn()}>Log In</Link>
+                    <Button
+                      as={Link}
+                      to={routes.signUp()}
+                      variant={'solid'}
+                      colorScheme={'teal'}
+                      size={'sm'}
+                    >
+                      Sign Up
+                    </Button>
+                  </>
+                )}
+              </HStack>
+            )}
           </Flex>
         </Flex>
       </Box>
