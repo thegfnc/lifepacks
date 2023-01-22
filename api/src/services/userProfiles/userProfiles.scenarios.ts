7fdb0d4dc34b1f6ac8a6b80ad03c27a49877050b
Jason Desiderio <jmdesiderio@gmail.com>
2023-01-22 00:17:09 -0600

    Fix all broken tests

--- api/src/services/userProfiles/userProfiles.scenarios.ts
+++ api/src/services/userProfiles/userProfiles.scenarios.ts
@@ -1,18 +1,21 @@
 import type { Prisma, UserProfile } from '@prisma/client'
+
 import type { ScenarioData } from '@redwoodjs/testing/api'
 
 export const standard = defineScenario<Prisma.UserProfileCreateArgs>({
   userProfile: {
     one: {
       data: {
-        userId: 'String9952131',
-        username: 'String805617',
+        userId: '4c18c0e2-568a-4da9-a785-3d5b1343abdb',
+        username: 'jmdesiderio',
+        givenName: 'Jason',
+        familyName: 'Desiderio',
         updatedAt: '2023-01-21T06:28:17.296Z',
       },
     },
     two: {
       data: {
-        userId: 'String6067388',
+        userId: '93893d12-bbd4-4784-8f56-6cda6e21b169',
         username: 'String8349404',
         updatedAt: '2023-01-21T06:28:17.296Z',
       },
