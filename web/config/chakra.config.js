fe534d6eaee34604f9dd27592579bd99ec50ecb2
Jason Desiderio <jmdesiderio@gmail.com>
2023-02-25 11:39:23 -0600

    Upgrade redwood to 4.2.0

--- web/config/chakra.config.js
+++ web/config/chakra.config.js
@@ -1,3 +1,18 @@
 // This object will be used to override Chakra-UI theme defaults.
 // See https://chakra-ui.com/docs/styled-system/theming/theme for theming options
-module.exports = {}
+module.exports = {
+  fonts: {
+    heading: `Rubik, sans-serif`,
+    body: `Rubik, sans-serif`,
+  },
+  // colors: {
+  //   cream: '#F4EBD2',
+  // },
+  // styles: {
+  //   global: {
+  //     body: {
+  //       bg: 'cream',
+  //     },
+  //   },
+  // },
+}
