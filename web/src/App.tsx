9bfd0ab7cfb6c098f8cef364b374f3607efdbbf4
Jason Desiderio <jmdesiderio@gmail.com>
2023-03-13 01:10:15 -0500

    Add error reporting to the frontend (#85)

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,4 +1,6 @@
 import { ChakraProvider, ColorModeScript, extendTheme } from '@chakra-ui/react'
+import * as Sentry from '@sentry/react'
+import { BrowserTracing } from '@sentry/tracing'
 import * as theme from 'config/chakra.config'
 
 import { FatalErrorBoundary, RedwoodProvider } from '@redwoodjs/web'
@@ -11,6 +13,12 @@ import Routes from 'src/Routes'
 import { AuthProvider, useAuth } from './auth'
 
 import './index.css'
+
+Sentry.init({
+  dsn: 'https://344cc4d839624898a7b07d84c1650f34@o1064557.ingest.sentry.io/4504829684547584',
+  integrations: [new BrowserTracing()],
+  tracesSampleRate: 1.0,
+})
 
 const extendedTheme = extendTheme(theme)
 
