7dcba9e26fb8b4706e6242c26138b84d80c78e5d
Jason Desiderio <jmdesiderio@gmail.com>
2023-03-27 23:30:54 -0500

    Try switching the order of the tests

--- api/src/services/packs/packs.scenarios.ts
+++ api/src/services/packs/packs.scenarios.ts
@@ -3,26 +3,6 @@ import type { Prisma, Pack } from '@prisma/client'
 import type { ScenarioData } from '@redwoodjs/testing/api'
 
 export const standard = defineScenario<Prisma.PackCreateArgs>({
-  pack: {
-    camping: {
-      data: {
-        userId: '6b555fae-0bf4-448a-b467-db072d608117',
-        slug: 'camping',
-        title: 'Camping',
-        description: 'String',
-        updatedAt: '2023-02-13T15:55:47.365Z',
-      },
-    },
-    computers: {
-      data: {
-        userId: '6b555fae-0bf4-448a-b467-db072d608117',
-        slug: 'computers',
-        title: 'Computers!',
-        description: 'String',
-        updatedAt: '2023-02-13T15:55:47.365Z',
-      },
-    },
-  },
   userProfile: {
     one: {
       data: {
@@ -40,6 +20,26 @@ export const standard = defineScenario<Prisma.PackCreateArgs>({
         givenName: 'Some',
         familyName: 'Otherguy',
         updatedAt: '2023-01-21T06:28:17.296Z',
+      },
+    },
+  },
+  pack: {
+    camping: {
+      data: {
+        userId: '6b555fae-0bf4-448a-b467-db072d608117',
+        slug: 'camping',
+        title: 'Camping',
+        description: 'String',
+        updatedAt: '2023-02-13T15:55:47.365Z',
+      },
+    },
+    computers: {
+      data: {
+        userId: '6b555fae-0bf4-448a-b467-db072d608117',
+        slug: 'computers',
+        title: 'Computers!',
+        description: 'String',
+        updatedAt: '2023-02-13T15:55:47.365Z',
       },
     },
   },
