cc327b81500addfb650b9ea257665f9401512720
Jason Desiderio <jmdesiderio@gmail.com>
2023-03-27 23:26:28 -0500

    Tweak one user id

--- api/src/services/packItems/packItems.scenarios.ts
+++ api/src/services/packItems/packItems.scenarios.ts
@@ -6,7 +6,7 @@ export const standard = defineScenario<Prisma.PackItemCreateArgs>({
   userProfile: {
     one: {
       data: {
-        userId: '6b555fae-0bf4-448a-b467-db072d608117',
+        userId: '6b555fae-0bf4-448a-b467-db072d608116',
         username: 'cooldude',
         givenName: 'Jason',
         familyName: 'Desiderio',
@@ -17,7 +17,7 @@ export const standard = defineScenario<Prisma.PackItemCreateArgs>({
   packItem: {
     one: {
       data: {
-        userId: '6b555fae-0bf4-448a-b467-db072d608117',
+        userId: '6b555fae-0bf4-448a-b467-db072d608116',
         title: 'String',
         purchaseUrl: 'String',
         imageUrl: 'String',
@@ -26,7 +26,7 @@ export const standard = defineScenario<Prisma.PackItemCreateArgs>({
         updatedAt: '2023-02-13T15:56:29.208Z',
         pack: {
           create: {
-            userId: '6b555fae-0bf4-448a-b467-db072d608117',
+            userId: '6b555fae-0bf4-448a-b467-db072d608116',
             slug: 'cooking',
             title: 'Cooking',
             description: 'String',
@@ -37,7 +37,7 @@ export const standard = defineScenario<Prisma.PackItemCreateArgs>({
     },
     two: {
       data: {
-        userId: '6b555fae-0bf4-448a-b467-db072d608117',
+        userId: '6b555fae-0bf4-448a-b467-db072d608116',
         title: 'String',
         purchaseUrl: 'String',
         imageUrl: 'String',
@@ -46,7 +46,7 @@ export const standard = defineScenario<Prisma.PackItemCreateArgs>({
         updatedAt: '2023-02-13T15:56:29.208Z',
         pack: {
           create: {
-            userId: '6b555fae-0bf4-448a-b467-db072d608117',
+            userId: '6b555fae-0bf4-448a-b467-db072d608116',
             slug: 'music',
             title: 'Music',
             description: 'String',
