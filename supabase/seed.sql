c33f81f5d29c0d93c12474d4a698f168410d8492
Jason Desiderio <jmdesiderio@gmail.com>
2023-04-02 13:05:09 -0500

    Add storage buckets to supabase seed file

--- supabase/seed.sql
+++ supabase/seed.sql
@@ -1,3 +1,32 @@
 /* Prisma requires a shadow database - https://supabase.com/docs/guides/integrations/prisma */
 
 CREATE DATABASE postgres_shadow;
+CREATE DATABASE postgres_test;
+
+
+
+insert into storage.buckets
+  (id, name)
+values
+  ('user-profile-images', 'user-profile-images');
+
+insert into storage.buckets
+  (id, name)
+values
+  ('pack-item-images', 'pack-item-images');
+
+create policy "Users can manipulate their own images (select)"
+  on storage.objects for select
+  using (auth.uid() = owner);
+
+create policy "Users can manipulate their own images (insert)"
+  on storage.objects for insert
+  with check (auth.uid() = owner);
+
+create policy "Users can manipulate their own images (update)"
+  on storage.objects for update
+  using (auth.uid() = owner);
+
+create policy "Users can manipulate their own images (delete)"
+  on storage.objects for delete
+  using (auth.uid() = owner);
