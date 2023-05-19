a5f8d7629af87c0d534bda3c4d326f22465e4003
Jason Desiderio <jmdesiderio@gmail.com>
2023-05-19 15:58:05 -0600

    Set Sentry user on the frontend as well

--- web/src/layouts/AppLayout/AppLayout.tsx
+++ web/src/layouts/AppLayout/AppLayout.tsx
@@ -1,15 +1,23 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 
+import { useAuth } from 'src/auth'
 import Footer from 'src/components/Footer/Footer'
 import Header from 'src/components/Header/Header'
 import HeaderCtaContext from 'src/contexts/HeaderCtaContext'
+import Sentry from 'src/lib/sentry'
 
 type AppLayoutProps = {
   children?: React.ReactNode
 }
 
 const AppLayout = ({ children }: AppLayoutProps) => {
+  const { currentUser } = useAuth()
   const [HeaderCtaComponent, setHeaderCtaComponent] = useState(null)
+
+  useEffect(
+    () => Sentry.setUser({ id: currentUser.sub, email: currentUser.email }),
+    [currentUser]
+  )
 
   return (
     <HeaderCtaContext.Provider value={setHeaderCtaComponent}>
