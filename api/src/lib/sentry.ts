22dc4acecda229977c92d005fc848a7f684cc603
Jason Desiderio <jmdesiderio@gmail.com>
2023-05-19 14:41:54 -0600

    Add sentry profiling to api

--- api/src/lib/sentry.ts
+++ api/src/lib/sentry.ts
@@ -1,4 +1,5 @@
 import * as Sentry from '@sentry/node'
+import { ProfilingIntegration } from '@sentry/profiling-node'
 
 // RedwoodJS + Sentry Inspiration:
 // https://gist.github.com/rockymeza/7dec7ddb435a6851e6e27d40b1ad0c1a#file-sentry-js-L5-L13
@@ -15,9 +16,11 @@ if (
     release: process.env.VERCEL_GIT_COMMIT_SHA,
     environment: process.env.VERCEL_ENV,
     tracesSampleRate: 0.1,
+    profilesSampleRate: 0.1,
     integrations: [
       // Automatically instrument Node.js libraries and frameworks
       ...Sentry.autoDiscoverNodePerformanceMonitoringIntegrations(),
+      new ProfilingIntegration(),
     ],
   })
   isSentryInitialized = true
