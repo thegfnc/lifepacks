b0377cf5d76a6dca0fdb539503c906b2bb94a63d
Jason Desiderio <jmdesiderio@gmail.com>
2023-06-06 16:45:19 -0500

    Upgrade some sentry deps

--- web/config/webpack.config.js
+++ web/config/webpack.config.js
@@ -1,4 +1,4 @@
-const SentryWebpackPlugin = require('@sentry/webpack-plugin')
+const { sentryWebpackPlugin } = require('@sentry/webpack-plugin')
 
 /** @returns {import('webpack').Configuration} Webpack Configuration */
 module.exports = (config, { mode }) => {
@@ -12,19 +12,13 @@ module.exports = (config, { mode }) => {
   // Add custom plugins for your project
   if (mode !== 'development') {
     config.plugins.push(
-      new SentryWebpackPlugin({
+      sentryWebpackPlugin({
         org: process.env.SENTRY_ORG,
         project: process.env.SENTRY_WEB_PROJECT,
-
-        // Specify the directory containing build artifacts
-        include: config.output.path,
 
         // Auth tokens can be obtained from https://sentry.io/settings/account/api/auth-tokens/
         // and needs the `project:releases` and `org:read` scopes
         authToken: process.env.SENTRY_AUTH_TOKEN,
-
-        // Optionally uncomment the line below to override automatic release name detection
-        release: process.env.VERCEL_GIT_COMMIT_SHA,
       })
     )
   }
