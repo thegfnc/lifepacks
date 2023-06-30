03fe31e685ae9eea8a397d9f7d4c5ac914731855
Jason Desiderio <jmdesiderio@gmail.com>
2023-06-29 22:57:01 -0500

    Fix lint bug

--- web/src/Routes.tsx
+++ web/src/Routes.tsx
@@ -7,8 +7,6 @@
 // 'src/pages/HomePage/HomePage.js'         -> HomePage
 // 'src/pages/Admin/BooksPage/BooksPage.js' -> AdminBooksPage
 
-import { StrictMode } from 'react'
-
 import { Router, Route, Set } from '@redwoodjs/router'
 
 import AppLayout from 'src/layouts/AppLayout/AppLayout'
@@ -23,41 +21,39 @@ const Routes = () => {
 
   return (
     <Router useAuth={useAuth}>
-      <StrictMode>
-        <Set wrap={AppLayout}>
-          {/* Marketing Pages */}
-          <Route path="/" page={HomePage} name="home" prerender />
-          <Route path="/explore" page={ExplorePage} name="explore" prerender />
+      <Set wrap={AppLayout}>
+        {/* Marketing Pages */}
+        <Route path="/" page={HomePage} name="home" prerender />
+        <Route path="/explore" page={ExplorePage} name="explore" prerender />
 
-          {/* Public Profile and Pack Pages */}
-          <Route path="/@{username}" page={UserProfilePage} name="userProfile" prerender />
-          <Route path="/@{username}/pack/{slug}" page={PackPage} name="pack" prerender />
+        {/* Public Profile and Pack Pages */}
+        <Route path="/@{username}" page={UserProfilePage} name="userProfile" prerender />
+        <Route path="/@{username}/pack/{slug}" page={PackPage} name="pack" prerender />
 
-          {/* Public Auth Pages */}
-          <Route path="/log-in" page={LogInPage} name="logIn" prerender />
-          <Route path="/sign-up" page={SignUpPage} name="signUp" prerender />
-          <Route path="/forgot-password" page={ForgotPasswordPage} name="forgotPassword" prerender />
+        {/* Public Auth Pages */}
+        <Route path="/log-in" page={LogInPage} name="logIn" prerender />
+        <Route path="/sign-up" page={SignUpPage} name="signUp" prerender />
+        <Route path="/forgot-password" page={ForgotPasswordPage} name="forgotPassword" prerender />
 
-          {/* Help Pages */}
-          <Route path="/help/affiliate-links-101" page={AffiliateLinks101Page} name="affiliateLinks101" prerender />
-          <Route path="/help/faq" page={FaqPage} name="faq" prerender />
-          {/* Legal Pages */}
-          <Route path="/privacy-policy" page={PrivacyPolicyPage} name="privacyPolicy" prerender />
-          <Route path="/terms-of-service" page={TermsOfServicePage} name="termsOfService" prerender />
+        {/* Help Pages */}
+        <Route path="/help/affiliate-links-101" page={AffiliateLinks101Page} name="affiliateLinks101" prerender />
+        <Route path="/help/faq" page={FaqPage} name="faq" prerender />
+        {/* Legal Pages */}
+        <Route path="/privacy-policy" page={PrivacyPolicyPage} name="privacyPolicy" prerender />
+        <Route path="/terms-of-service" page={TermsOfServicePage} name="termsOfService" prerender />
 
-          {/* Private Dashboard Pages*/}
-          <Set private unauthenticated="home">
-            <Route path="/me/pack/edit/{id:Int}" page={EditPackPage} name="editPack" />
-            <Route path="/me/pack/new" page={NewPackPage} name="newPack" />
+        {/* Private Dashboard Pages*/}
+        <Set private unauthenticated="home">
+          <Route path="/me/pack/edit/{id:Int}" page={EditPackPage} name="editPack" />
+          <Route path="/me/pack/new" page={NewPackPage} name="newPack" />
 
-            <Route path="/me/reset-password" page={ResetPasswordPage} name="resetPassword" />
-            <Route path="/me/complete-sign-up" page={CompleteSignUpPage} name="completeSignUp" />
-          </Set>
+          <Route path="/me/reset-password" page={ResetPasswordPage} name="resetPassword" />
+          <Route path="/me/complete-sign-up" page={CompleteSignUpPage} name="completeSignUp" />
         </Set>
+      </Set>
 
-        {/* System Pages */}
-        <Route notfound page={NotFoundPage} prerender />
-      </StrictMode>
+      {/* System Pages */}
+      <Route notfound page={NotFoundPage} prerender />
     </Router>
   )
 }
