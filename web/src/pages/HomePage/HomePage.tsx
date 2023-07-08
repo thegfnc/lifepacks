d8f7dfcf4fe34aed1c778ddaca5a4bc3a5daea7b
Jason Desiderio <jmdesiderio@gmail.com>
2023-07-08 13:24:27 -0500

    Test error

--- web/src/pages/HomePage/HomePage.tsx
+++ web/src/pages/HomePage/HomePage.tsx
@@ -38,6 +38,14 @@ const HomePage = () => {
     <>
       <MetaTags ogUrl={getEnvironmentUrl('/')} />
 
+      <Button
+        onClick={() => {
+          throw new Error('test sentry user feedback')
+        }}
+      >
+        Test Error
+      </Button>
+
       <Box as="section" bg="brown.500" overflow="hidden">
         <PageContainer
           pt={0}
