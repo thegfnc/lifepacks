fe8a0c4acf03eb5792b6a65a9949ef67b20819c0
Jason Desiderio <jmdesiderio@gmail.com>
2023-08-07 23:48:23 -0500

    Start to make homepage responsive

--- web/src/pages/HomePage/HomePage.tsx
+++ web/src/pages/HomePage/HomePage.tsx
@@ -42,10 +42,10 @@ const HomePage = () => {
           pb={0}
           px={0} // moved paddings to Stack below so box-shadow doesn't cut off
           maxHeight={{ base: '150vh', md: 'calc(100vh - 110px)' }}
-          minHeight={{ base: 'auto', md: '630px' }}
+          minHeight={{ base: '350px', md: '630px' }}
         >
           <Stack
-            direction={{ base: 'column', lg: 'row' }}
+            direction="row"
             overflow="hidden"
             height="full"
             px={{ base: 4, md: 10 }}
@@ -56,51 +56,28 @@ const HomePage = () => {
               <Box
                 w={'full'}
                 maxW={{ base: '760px', lg: '400px', xl: '448px' }}
-                textAlign={{ base: 'center', lg: 'left' }}
+                textAlign="left"
                 mr={{ base: 0, lg: '40px', xl: '72px' }}
               >
-                <Show above="lg">
-                  <Heading
-                    fontSize={{ lg: '48px', xl: '56px' }}
-                    lineHeight={1}
-                    letterSpacing="tighter"
-                    color="marketing.deepBlue"
-                    fontWeight={800}
-                  >
-                    <Balancer>Recommend the products you swear by.</Balancer>
-                  </Heading>
-                  <Text
-                    fontFamily="bitter"
-                    fontSize={{ base: 'lg', md: '2xl' }}
-                    lineHeight="1.33"
-                    mt={{ base: 4, md: 6 }}
-                    color="marketing.deepBlue"
-                  >
-                    Easily create product guides and earn commission–just like
-                    the pros at <i>Wirecutter</i> and <i>Consumer Reports</i>.
-                  </Text>
-                </Show>
-                <Hide above="lg">
-                  <Heading
-                    fontSize={{ base: '36px', md: '48px' }}
-                    lineHeight={1}
-                    letterSpacing="tighter"
-                    color="marketing.deepBlue"
-                    fontWeight={800}
-                  >
-                    <Balancer>Recommend the products you swear by.</Balancer>
-                  </Heading>
-                  <Text
-                    fontFamily="bitter"
-                    fontSize={{ base: '18px', md: '2xl' }}
-                    lineHeight="1.33"
-                    mt={{ base: 4 }}
-                    color="marketing.deepBlue"
-                  >
-                    Easily create product guides and earn commission–just like
-                    the pros at <i>Wirecutter</i> and <i>Consumer Reports</i>.
-                  </Text>
-                </Hide>
+                <Heading
+                  fontSize={{ base: '48px', xl: '56px' }}
+                  lineHeight={1}
+                  letterSpacing="tighter"
+                  color="marketing.deepBlue"
+                  fontWeight={800}
+                >
+                  <Balancer>Recommend the products you swear by.</Balancer>
+                </Heading>
+                <Text
+                  fontFamily="bitter"
+                  fontSize={{ base: '2xl', md: '2xl' }}
+                  lineHeight="1.33"
+                  mt={{ base: 4, md: 6 }}
+                  color="marketing.deepBlue"
+                >
+                  Easily create product guides and earn commission–just like the
+                  pros at <i>Wirecutter</i> and <i>Consumer Reports</i>.
+                </Text>
                 <Box mt={{ base: 6, lg: 10 }}>
                   <HStack spacing={3}>
                     <Button
@@ -123,32 +100,34 @@ const HomePage = () => {
                 </Box>
               </Box>
             </Flex>
-            <Flex
-              flex={1}
-              justify="center"
-              mt={{ base: 8, md: 10, lg: 0 }}
-              position="relative"
-              pointerEvents="none"
-              userSelect="none"
-            >
-              <Stack
-                spacing={{ base: 4, md: 6 }}
-                maxW={{ base: '440px', md: '760px', lg: 'none' }}
+            <Show above="lg">
+              <Flex
+                flex={1}
+                justify="center"
+                mt={{ base: 8, md: 10, lg: 0 }}
+                position="relative"
+                pointerEvents="none"
+                userSelect="none"
               >
-                <PackItem
-                  title="KitchenAid Artisan Series Tilt Head Stand Mixer"
-                  description="This was my gateway into baking. Take your cookies and cakes to the next level."
-                  purchaseUrl="https://amzn.to/"
-                  imageUrl="https://lewdorirqeadvphajbbq.supabase.co/storage/v1/render/image/public/pack-item-images/2ebd0064-d1e8-414f-a008-90eaf26eac63-kitchenaid.png?height=700&width=700&resize=contain"
-                />
-                <PackItem
-                  title="Le Creuset Enameled Cast Iron Dutch Oven, 2.75 Qt"
-                  description="A beautiful, versatile kitchen staple for stews, sauces, bread and more."
-                  purchaseUrl="https://goto.walmart.com/"
-                  imageUrl="https://lewdorirqeadvphajbbq.supabase.co/storage/v1/render/image/public/pack-item-images/a0944bd5-16f6-473c-bc6a-91eb90cf42b1-dutchoven.png?height=700&width=700&resize=contain"
-                />
-              </Stack>
-            </Flex>
+                <Stack
+                  spacing={{ base: 4, md: 6 }}
+                  maxW={{ base: '440px', md: '760px', lg: 'none' }}
+                >
+                  <PackItem
+                    title="KitchenAid Artisan Series Tilt Head Stand Mixer"
+                    description="This was my gateway into baking. Take your cookies and cakes to the next level."
+                    purchaseUrl="https://amzn.to/"
+                    imageUrl="https://lewdorirqeadvphajbbq.supabase.co/storage/v1/render/image/public/pack-item-images/2ebd0064-d1e8-414f-a008-90eaf26eac63-kitchenaid.png?height=700&width=700&resize=contain"
+                  />
+                  <PackItem
+                    title="Le Creuset Enameled Cast Iron Dutch Oven, 2.75 Qt"
+                    description="A beautiful, versatile kitchen staple for stews, sauces, bread and more."
+                    purchaseUrl="https://goto.walmart.com/"
+                    imageUrl="https://lewdorirqeadvphajbbq.supabase.co/storage/v1/render/image/public/pack-item-images/a0944bd5-16f6-473c-bc6a-91eb90cf42b1-dutchoven.png?height=700&width=700&resize=contain"
+                  />
+                </Stack>
+              </Flex>
+            </Show>
           </Stack>
         </PageContainer>
       </Box>
@@ -171,58 +150,13 @@ const HomePage = () => {
               </Box>
             </Box>
           </Box>
-          <Box minW="320px" maxW="320px" ml="80px">
+          <Box minW="320px" maxW="320px" ml={{ base: '48px', xl: '80px' }}>
             <Heading fontSize="xl" fontWeight={700} lineHeight={1.33}>
               Featured Members
             </Heading>
             <Box mt={2}>
               <UserProfilesFeaturedCell />
             </Box>
-            <Heading fontSize="xl" fontWeight={700} lineHeight={1.33} mt={14}>
-              Topics
-            </Heading>
-            <Wrap mt={4} spacing={3} color="blackAlpha.900">
-              <Button
-                variant="outline"
-                size="md"
-                colorScheme="gray"
-                fontWeight={400}
-              >
-                Fitness
-              </Button>
-              <Button
-                variant="outline"
-                size="md"
-                colorScheme="gray"
-                fontWeight={400}
-              >
-                Entertainment
-              </Button>
-              <Button
-                variant="outline"
-                size="md"
-                colorScheme="gray"
-                fontWeight={400}
-              >
-                Music
-              </Button>
-              <Button
-                variant="outline"
-                size="md"
-                colorScheme="gray"
-                fontWeight={400}
-              >
-                Photography
-              </Button>
-              <Button
-                variant="outline"
-                size="md"
-                colorScheme="gray"
-                fontWeight={400}
-              >
-                Technology
-              </Button>
-            </Wrap>
           </Box>
         </Flex>
       </PageContainer>
