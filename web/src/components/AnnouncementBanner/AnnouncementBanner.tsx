0783877f4d4cc8e96324253c3bebf4002201c3bb
Jason Desiderio <jmdesiderio@gmail.com>
2023-08-22 16:15:21 -0500

    Allow users to dismiss announcement banner

--- web/src/components/AnnouncementBanner/AnnouncementBanner.tsx
+++ web/src/components/AnnouncementBanner/AnnouncementBanner.tsx
@@ -1,11 +1,13 @@
 import {
   Box,
   Button,
+  CloseButton,
   Grid,
   GridItem,
   Heading,
   Image,
   Text,
+  useBoolean,
 } from '@chakra-ui/react'
 
 import { Link, routes } from '@redwoodjs/router'
@@ -15,11 +17,35 @@ import PageContainer from '../PageContainer/PageContainer'
 import announcementBannerIcons from './announcement-banner-icons.png'
 
 const BANNER_HEIGHT = '245px'
+const BANNER_VERSION = 1 // bump this each time you want to show the banner again
+const BANNER_LOCAL_STORAGE_KEY = 'announcementBannerDismissedVersion'
 
 const AnnouncementBanner = () => {
-  return (
+  const [hasJustDismissedBanner, setHasJustDismissedBanner] = useBoolean(false)
+  const hasAlreadyDismissedBanner =
+    localStorage.getItem(BANNER_LOCAL_STORAGE_KEY) === BANNER_VERSION.toString()
+
+  const hasDismissedBanner = hasJustDismissedBanner || hasAlreadyDismissedBanner
+
+  const handleDismissBanner = () => {
+    setHasJustDismissedBanner.on()
+    localStorage.setItem(BANNER_LOCAL_STORAGE_KEY, BANNER_VERSION.toString())
+  }
+
+  return hasDismissedBanner ? null : (
     <PageContainer pt={10} pb={0} minHeight="none">
-      <Box background="purple.50" height={BANNER_HEIGHT} borderRadius="3xl">
+      <Box
+        background="purple.50"
+        height={BANNER_HEIGHT}
+        borderRadius="3xl"
+        position="relative"
+      >
+        <CloseButton
+          position="absolute"
+          top={6}
+          right={6}
+          onClick={handleDismissBanner}
+        />
         <Grid templateColumns="repeat(2, minmax(0, 1fr))">
           <GridItem px={10} display="flex" alignItems="center">
             <Box>
