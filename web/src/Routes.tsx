70a656e47d944a537f570aa5bc552752902bef20
Jason Desiderio <jason@thegoodfornothings.club>
2024-02-06 12:29:05 -0600

    Attempt to add some prerenders back

--- web/src/Routes.tsx
+++ web/src/Routes.tsx
@@ -7,7 +7,7 @@
 // 'src/pages/HomePage/HomePage.js'         -> HomePage
 // 'src/pages/Admin/BooksPage/BooksPage.js' -> AdminBooksPage
 
-import { Router, Route, Set } from '@redwoodjs/router'
+import { Router, Route, Set, PrivateSet } from '@redwoodjs/router'
 
 import AppLayout from 'src/layouts/AppLayout/AppLayout'
 
@@ -23,34 +23,34 @@ const Routes = () => {
     <Router useAuth={useAuth}>
       <Set wrap={AppLayout}>
         {/* Marketing Pages */}
-        <Route path="/" page={HomePage} name="home" />
-        <Route path="/about" page={AboutPage} name="about" />
+        <Route path="/" page={HomePage} name="home" prerender />
+        <Route path="/about" page={AboutPage} name="about" prerender />
 
         {/* Public Profile and Pack Pages */}
         <Route path="/u/{username}" page={UserProfilePage} name="userProfile" />
         <Route path="/u/{username}/pack/{slug}" page={PackPage} name="pack" />
 
         {/* Public Auth Pages */}
-        <Route path="/log-in" page={LogInPage} name="logIn" />
-        <Route path="/sign-up" page={SignUpPage} name="signUp" />
-        <Route path="/forgot-password" page={ForgotPasswordPage} name="forgotPassword" />
+        <Route path="/log-in" page={LogInPage} name="logIn" prerender />
+        <Route path="/sign-up" page={SignUpPage} name="signUp" prerender />
+        <Route path="/forgot-password" page={ForgotPasswordPage} name="forgotPassword" prerender />
 
         {/* Help Pages */}
-        <Route path="/help/affiliate-links-101" page={HelpAffiliateLinks101Page} name="affiliateLinks101" />
-        <Route path="/help/faq" page={HelpFaqPage} name="faq" />
+        <Route path="/help/affiliate-links-101" page={HelpAffiliateLinks101Page} name="affiliateLinks101" prerender />
+        <Route path="/help/faq" page={HelpFaqPage} name="faq" prerender />
 
         {/* Legal Pages */}
-        <Route path="/privacy-policy" page={LegalPrivacyPolicyPage} name="privacyPolicy" />
-        <Route path="/terms-of-service" page={LegalTermsOfServicePage} name="termsOfService" />
+        <Route path="/privacy-policy" page={LegalPrivacyPolicyPage} name="privacyPolicy" prerender />
+        <Route path="/terms-of-service" page={LegalTermsOfServicePage} name="termsOfService" prerender />
 
         {/* Private Dashboard Pages*/}
-        <Set private unauthenticated="home">
+        <PrivateSet unauthenticated="home">
           <Route path="/me/pack/edit/{id:Int}" page={EditPackPage} name="editPack" />
           <Route path="/me/pack/new" page={NewPackPage} name="newPack" />
 
           <Route path="/me/reset-password" page={ResetPasswordPage} name="resetPassword" />
           <Route path="/me/complete-sign-up" page={CompleteSignUpPage} name="completeSignUp" />
-        </Set>
+        </PrivateSet>
       </Set>
 
       {/* System Pages */}
